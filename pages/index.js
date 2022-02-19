58df310a514d0cb9f11c83300732d53e1fd76e1c
ryuyaF <r.fukushima403@gmail.com>
2022-02-19 12:36:40 +0900

    一旦、TOPページへの遷移はitemsへページ遷移する

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,10 @@
-import Image from 'next/image'
-import ListOutlinedIcon from '@mui/icons-material/ListOutlined'
-import ReplayOutlinedIcon from '@mui/icons-material/ReplayOutlined'
-import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined'
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
-import Link from 'next/link'
-
-const styles = {
-  mainBGColor: '#FAFAFE',
-  sideBarBGColor: '#FFFFFF',
-  cartBGColor: '#F9A109'
-}
+import Router from "next/router"
+import { useEffect } from "react"
 
 export default function Home() {
-  return (
-    <>
-      TOP
-    </>
-  )
+  useEffect(() => {
+    Router.push('/items')
+  }, [])
+
+  return <></>
 }
