b1ed8f75fff2a4abaf509e1d324a0ca04b15fa03
ryuyaF <r.fukushima403@gmail.com>
2022-03-05 16:36:38 +0900

    サイドバーナビゲーションのツールチップを実装

---
+++ components/navigators/navigate-icon.js
@@ -0,0 +1,56 @@
+import { useRef } from 'react'
+import PropTypes from 'prop-types'
+import css from 'styled-jsx/css'
+
+const styles = css`
+  .tooltip {
+    background-color: #454545;
+  }
+`
+
+const NavigateIcon = (props) => {
+  const ref = useRef(null)
+
+  const handleMouseEnter = () => {
+    if (!ref.current) return;
+
+    ref.current.style.opacity = 1
+    ref.current.style.marginLeft = '20px'
+  }
+
+  const handleMouseLeave = () => {
+    if (!ref.current) return;
+
+    ref.current.style.opacity = 0
+    ref.current.style.marginLeft = '10px'
+  }
+
+  return (
+    <div
+      className='relative flex items-center'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <style jsx>{styles}</style>
+      <div
+        className="tooltip absolute whitespace-nowrap text-white px-4 py-2 rounded flex items-center transition-all duration-150"
+        style={{left: '100%', opacity: 0}}
+        ref={ref}
+      >
+        <div
+          className='tooltip h-3 w-3 absolute'
+          style={{ left: '-6px', transform: 'rotate(45deg)'}}
+        />
+        {props.text}
+      </div>
+      {props.children}
+    </div>
+  )
+}
+
+NavigateIcon.propTypes = {
+  text: PropTypes.string,
+  children: PropTypes.node
+}
+
+export default NavigateIcon
--- pages/_app.js
+++ pages/_app.js
@@ -5,41 +5,56 @@ import ReplayOutlinedIcon from '@mui/icons-material/ReplayOutlined'
 import InsertChartOutlinedIcon from '@mui/icons-material/InsertChartOutlined'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import Link from 'next/link'
+import NavigateIcon from '../components/navigators/navigate-icon'
+import css from 'styled-jsx/css'
 
-const styles = {
-  mainBGColor: '#FAFAFE',
-  sideBarBGColor: '#FFFFFF',
-  cartBGColor: '#F9A109'
-}
+const styles = css`
+  .main-bg-color {
+    background-color: #FAFAFE;
+  }
+  .side-bar-bg-color {
+    background-color: #FFFFFF;
+  }
+  .cart-bg-color {
+    background-color: #F9A109;
+  }
+`
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className='min-w-full min-h-screen rounded-xl flex' style={{backgroundColor: styles.mainBGColor}}>
+      <style jsx>{styles}</style>
+      <div className='min-w-full min-h-screen rounded-xl flex main-bg-color'>
         {/* 1列目 サイドメニュー */}
-        <div className='w-24 min-h-screen flex flex-col justify-between' style={{backgroundColor: styles.sideBarBGColor}}>
+        <div className='w-24 min-h-screen flex flex-col justify-between side-bar-bg-color'>
           <div className='flex-1 flex items-start justify-center mt-8'>
             <Image src={'/pictures/logo.svg'} alt='Picture of the logo' width={40} height={40}/>
           </div>
           <ul className="flex-1 flex flex-col justify-around items-center">
             <li className='-mb-px mr-1'>
-              <Link href="/items">
-                <a><ListOutlinedIcon sx={{ color: '#000' }} /></a>
-              </Link>
+              <NavigateIcon text='items'>
+                <Link href="/items">
+                  <a><ListOutlinedIcon sx={{ color: '#000' }} /></a>
+                </Link>
+              </NavigateIcon>
             </li>
             <li className="mr-1">
-              <Link href="/histories">
-                <a><ReplayOutlinedIcon sx={{ color: '#000' }} /></a>
-              </Link>
+              <NavigateIcon text='history'>
+                <Link href="/histories">
+                  <a><ReplayOutlinedIcon sx={{ color: '#000' }} /></a>
+                </Link>
+              </NavigateIcon>
             </li>
             <li className="mr-1">
-              <Link href="/statics">
-              <a><InsertChartOutlinedIcon sx={{ color: '#000' }} /></a>
-              </Link>
+              <NavigateIcon text='statics'>
+                <Link href="/statics">
+                <a><InsertChartOutlinedIcon sx={{ color: '#000' }} /></a>
+                </Link>
+              </NavigateIcon>
             </li>
           </ul>
           <div className='flex-1 flex flex-col justify-end'>
-            <div className='w-10 h-10 mx-auto rounded-full mb-8 flex flex-col justify-center items-center' style={{backgroundColor: styles.cartBGColor}}>
+            <div className='w-10 h-10 mx-auto rounded-full mb-8 flex flex-col justify-center items-center cart-bg-color'>
               <ShoppingCartOutlinedIcon sx={{ color: '#FFF', fontSize: 20 }} />
             </div>
           </div>
